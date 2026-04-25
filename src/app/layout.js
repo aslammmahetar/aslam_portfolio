cf41dc15fddf524f11c099f95b4a06d60d494a41
aslammmahetar <mmahetaraslam@gmail.com>
2026-04-25 19:31:14 +0530

    metadata added

--- src/app/layout.js
+++ src/app/layout.js
@@ -7,7 +7,7 @@ const inter = Inter({ subsets: ["latin"] })
 
 export const metadata = {
   title: "Aslam | Full Stack Developer",
-  description: "Full Stack Developer Portfolio",
+  description: "Building scalable web apps & automation tools",
 };
 
 export default function RootLayout({ children }) {
